c1e8dc46286ddea8d131bc69b92140ad7f175d6f
ryand67 <rwdevenney@gmail.com>
2021-06-23 22:12:46 -0400

    placeholder nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,20 @@ import React from 'react'
 import styled from 'styled-components';
 
 import { auth } from '../util/firebase';
+import { Link } from 'react-router-dom';
 
 function Nav() {
     return (
         <NavBar>
-            <button onClick={() => auth.signOut()}>Logout</button>
+            <Link to='/'>
+                <button>Home</button>
+            </Link>
+
+            <Link to='/create-bug'>
+                <button>New Bug</button>
+            </Link>
+
+                <button onClick={() => auth.signOut()}>Logout</button>
         </NavBar>
     )
 }
@@ -16,6 +25,13 @@ const NavBar = styled.nav`
     position: fixed;
     top: 0;
     padding-top: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    * {
+        margin: 0 1rem;
+    }
 `;
 
 export default Nav
