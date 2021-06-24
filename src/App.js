0851d911bda810eaf9597c5111ab0a31da85605b
ryand67 <rwdevenney@gmail.com>
2021-06-23 20:08:59 -0400

    comps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import Home from './components/Home';
+import Credentials from './components/Credentials';
+
+import { auth } from './util/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth'
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-
+      {user ? <Home /> : <Credentials />}
     </div>
   );
 }
