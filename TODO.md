688a915567bd2f58b6b877773c29395f055f2a4f
ryand67 <rwdevenney@gmail.com>
2021-06-24 18:46:46 -0400

    new post form fields and states

--- TODO.md
+++ TODO.md
@@ -3,4 +3,4 @@ Things to do:
 * Posts
 * Table
 
-Post properties: Poster, id, status, message, category, date
+Post properties: Poster, id, status, message, category, date, due date
--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function NewPostForm() {
+
+    const [dueDate, setDueDate] = useState('');
+    const [bugDesc, setBugDesc] = useState('');
+    const [category, setCategory] = useState('');
+    const [status, setStatus] = useState('');
+
     return (
         <form>
-            asdf
+            <label htmlFor="">Due Date:</label>
+            <input type="date" name="" id="" onChange={(e) => setDueDate(e.target.value)} />
+            <label htmlFor="">Bug Description:</label>
+            <textarea name="" id="" cols="30" rows="10" onChange={(e) => setBugDesc(e.target.value)}></textarea>
+            <label htmlFor="">Category</label>
+            <select name="" id="" onChange={(e) => setCategory(e.target.value)}>
+                <option value="Development">Development</option>
+                <option value="Human Resources">Human Resources</option>
+                <option value="Tech Support">Tech Support</option>
+            </select>
+            <label htmlFor="">Status:</label>
+            <select name="" id="" onChange={(e) => setStatus(e.target.value)}>
+                <option value="In Progress">In Progress</option>
+                <option value="Completed">Completed</option>
+                <option value="Unassigned">Unassigned</option>
+            </select>
+            <button type="submit">Submit Bug</button>
         </form>
     )
 }
