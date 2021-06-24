38a4d6d685efc9dea36a9ebada7074a7cab27374
ryand67 <rwdevenney@gmail.com>
2021-06-23 20:18:15 -0400

    components

--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import SignUp from './SignUp';
+import SignIn from './SignIn';
 
 function Credentials() {
+
+    const [signInFlag, setSignInFlag] = useState(false);
+
     return (
         <div>
-            
+            <button onClick={() => setSignInFlag(!signInFlag)}>{signInFlag ? 'Have an account? Sign In' : "Don't have an account? Sign Up"}</button>
         </div>
     )
 }
---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            qwer
+        </div>
+    )
+}
+
+export default SignIn
